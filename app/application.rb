92df40d3ef64d3265b3b33a63fad1d5f0e55d678
Rehman Ali <rehmanali001@gmail.com>
2019-09-09 15:26:28 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application 
+  
+  def call(env)
+    resp = Rack::Response.new 
+    
+    noon = Kernel.rand(1..24)
+    
+    if noon < 12 
+      resp.write "Good Morning!"
+    elsif noon >= 12 
+      resp.write "Good Afternoon!"
+    
+  end 
+   resp.finish 
+  end
+end 
