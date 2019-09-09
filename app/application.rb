8c0d9c1c1f9eea3baf3f8ee0ffe766fedbd575ff
Rehman Ali <rehmanali001@gmail.com>
2019-09-09 15:24:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     
-    noon = Kernel.rand(1..12)
+    noon = Kernel.rand(1..24)
     
     if noon < 12 
       resp.write "GoodMorning!"
-  
-  
+    else 
+    
   end
 end 
